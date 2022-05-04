21414204734cf22d4166af65095c098dd9971e36
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-05-04 15:41:51 +0200

    Add very little style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>Movies</title>
+
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
-    @foreach ($movies as $movie)
-        <div class="card">
-            <h2>{{ $movie->title }}</h2>
-            <h3>{{ $movie->original_title }}</h3>
-            <div>{{ $movie->nationality }}</div>
-            <p>{{ $movie->date }}</p>
-            <p>{{ $movie->vote }}</p>
-        </div>
-    @endforeach
-
+    <main>
+        @foreach ($movies as $movie)
+            <div class="card">
+                <h2>{{ $movie->title }}</h2>
+                <h3>{{ $movie->original_title }}</h3>
+                <div>{{ $movie->nationality }}</div>
+                <div>{{ date("d/m/Y", strtotime($movie->date)) }}</div>
+                <div>
+                  @for ($i= 0; $i < floor($movie->vote); $i++)
+                    &#11088;
+                  @endfor
+                </div>
+            </div>
+        @endforeach
+    </main>
 </body>
 </html>
